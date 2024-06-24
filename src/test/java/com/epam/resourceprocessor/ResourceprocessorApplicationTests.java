75c115d7693107491dc01e20124db60486f11430
Sherzod Omonov <sherzodbeek98@gmail.com>
2024-06-24 10:09:35 +0500

    .gitignore, gradle added

---
+++ src/test/java/com/epam/resourceprocessor/ResourceprocessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.resourceprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceprocessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
