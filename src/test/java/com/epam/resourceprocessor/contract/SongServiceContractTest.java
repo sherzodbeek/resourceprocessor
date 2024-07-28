c6009437cf313a96467edb06380fa202bbdec157
Sherzod Omonov <sherzodbeek98@gmail.com>
2024-07-28 23:26:56 +0500

    test prop added

--- src/test/java/com/epam/resourceprocessor/contract/SongServiceContractTest.java
+++ src/test/java/com/epam/resourceprocessor/contract/SongServiceContractTest.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
@@ -22,6 +23,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations = "classpath:application-test.properties")
 @AutoConfigureMockMvc
 @AutoConfigureJsonTesters
 @AutoConfigureStubRunner(
