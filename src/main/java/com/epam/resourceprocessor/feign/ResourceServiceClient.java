2d3b5f28c103be50c4b25d42ed86767402ae993f
Sherzod Omonov <sherzodbeek98@gmail.com>
2024-08-06 01:38:50 +0500

    Using gateway url in feign client

--- src/main/java/com/epam/resourceprocessor/feign/ResourceServiceClient.java
+++ src/main/java/com/epam/resourceprocessor/feign/ResourceServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "${resource.service.name}", path = "/api/resources")
+@FeignClient(value = "${resource.service.name}", url = "${gateway.url}", path = "/api/resources")
 public interface ResourceServiceClient {
 
     @GetMapping("/{id}")
--- src/main/java/com/epam/resourceprocessor/feign/SongServiceClient.java
+++ src/main/java/com/epam/resourceprocessor/feign/SongServiceClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(value = "${song.service.name}", path = "/api/songs")
+@FeignClient(value = "${song.service.name}", url = "${gateway.url}", path = "/api/songs")
 public interface SongServiceClient {
 
     @GetMapping("/{id}")
